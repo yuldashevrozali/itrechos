70aff4d36e45fc817ce4b9f59dff1e56c05fe69d
yuldashevrozali <yuldashevrozali08@gmail.com>
2024-10-20 20:24:05 +0500

    userd

--- src/pages/ServicesPage/index.jsx
+++ src/pages/ServicesPage/index.jsx
@@ -1,22 +1,101 @@
-import group from '../../assets/group.png'
+import group from "../../assets/group.png";
+import icon from "../../assets/icon.png";
+import icon2 from "../../assets/icon2.png";
+import icon3 from "../../assets/icon3.png";
+import right from "../../assets/right.png";
+import "./index.css";
 function Services() {
-    return(
-        <>
-        <div className="services">
-            <div className="services-left">
-                <h1>we Providing Complete Professional IT Services.</h1>
-                <p>Assertively streamline interactive interfaces after value a infrastructures. Authoritatively fabricate fully tested methodologies before Holistic deliver end-to-end architectures rather sound benefits.</p>
-                <div className="services-left-block1">
-                    <img src={group} alt="" />
-                </div>
-                <div className="services-left-block2"></div>
-                <div className="services-left-block3"></div>
+  return (
+    <div className="services">
+      <div className="services-blocks">
+        <div className="services-left">
+          <h1>We Providing Complete Professional IT Services.</h1>
+          <p>
+            Assertively streamline interactive interfaces after value a
+            infrastructures. Authoritatively fabricate fully tested
+            methodologies before Holistic deliver end-to-end architectures
+            rather sound benefits.
+          </p>
+          <div className="services-left-block1">
+            <img src={group} alt="" />
+            <div className="services-span">
+              <h1>Analytical Research Problem Findings</h1>
+              <p>
+                Assertively streamline interactive interfaces after value-added
+                infrastructures. Authoritatively fabricate fully tested
+                methodologies before.{" "}
+              </p>
             </div>
-            <div className="services-right"></div>
+          </div>
+          <div className="services-left-block1">
+            <img src={group} alt="" />
+            <div className="services-span">
+              <h1>Analytical Research Problem Findings</h1>
+              <p>
+                Assertively streamline interactive interfaces after value-added
+                infrastructures. Authoritatively fabricate fully tested
+                methodologies before.{" "}
+              </p>
+            </div>
+          </div>
+          <div className="services-left-block1">
+            <img src={group} alt="" />
+            <div className="services-span">
+              <h1>Analytical Research Problem Findings</h1>
+              <p>
+                Assertively streamline interactive interfaces after value-added
+                infrastructures. Authoritatively fabricate fully tested
+                methodologies before.{" "}
+              </p>
+            </div>
+          </div>
         </div>
-        </>
-    )
-    
+        <div className="services-right">
+          <div className="services-right-block srb1">
+            <img src={icon} alt="" />
+            <h2>Network solution</h2>
+            <p>
+              Assertively streamline interactive interfaces after value-added
+              infrastructures.
+            </p>
+            <div className="right-span">
+              <img src={right} alt="" />
+              <p>LEARN MORE</p>
+            </div>
+          </div>
+          <div className="services-right-block srb2">
+            <img src={icon2} alt="" />
+            <h2>Data analytics</h2>
+            <p>
+              Assertively streamline interactive interfaces after value-added
+              infrastructures.
+            </p>
+            <div className="right-span">
+              <img src={right} alt="" />
+              <p>LEARN MORE</p>
+            </div>
+          </div>
+          <div className="services-right-block srb3">
+            <img src={icon3} alt="" />
+            <h2>Cyber security</h2>
+            <p>
+              Assertively streamline interactive interfaces after value-added
+              infrastructures.
+            </p>
+            <div className="right-span">
+              <img src={right} alt="" />
+              <p>LEARN MORE</p>
+            </div>
+          </div>
+          <div className="services-right-block srb4">
+            <img src={right} alt="" />
+            <h1>View all services</h1>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
